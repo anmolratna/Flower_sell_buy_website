adf80f7dd441c006c8ef5a88b7fa519a64094bd7
anmolratna <114939466+anmolratna@users.noreply.github.com>
2024-07-11 12:41:36 +0530

    Delete Backend directory

--- Backend/custom_error/own_error.js
+++
@@ -1,9 +0,0 @@
-class ownerror_handler extends Error{
-    constructor(status , message){
-        super()
-        this.status=status;
-        this.message=message;
-    }
-}
-
-module.exports=ownerror_handler;
